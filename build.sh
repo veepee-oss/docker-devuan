7aa13367fa1540b1c3fb5f25350b4db182187b3b
Antoine MILLET <amillet@vente-privee.com>
2018-02-18 20:36:59 +0100

    Fix mirror

--- build.sh
+++ build.sh
@@ -347,12 +347,12 @@ then
         jessie|1|1.0)
             distname='jessie'
             distid='1'
-            mirror='http://mirror.vpgrp.io/merged'
+            mirror='http://mirror.vpgrp.io/devuan'
             ;;
         ascii|2|2.0)
             distname='stretch'
             distid='2'
-            mirror='http://mirror.vpgrp.io/merged'
+            mirror='http://mirror.vpgrp.io/devuan'
             include='gnupg2'
             ;;
         *)
